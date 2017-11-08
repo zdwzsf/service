b7fadcdfdf55fc9aeaf7ac0378a288a89b61f31f
paas <paas@cse-codegen-backend-1592449050-pkkb5>
2017-11-08 02:27:11 +0000

    Auto pushed by FusionStage!

--- src/main/java/com/services/pojo/csedemo/CsedemoImpl.java
+++ src/main/java/com/services/pojo/csedemo/CsedemoImpl.java
@@ -7,7 +7,7 @@ import javax.validation.constraints.*;
 import io.servicecomb.provider.pojo.RpcSchema;
 import com.services.pojo.csedemo.Csedemo;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T02:21:23.731Z")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CsePojoCodegen", date = "2017-11-08T02:27:11.793Z")
 
 @RpcSchema(schemaId = "csedemo")
 public class CsedemoImpl implements Csedemo {
